e417e9756d481911b8141290ae32dcf29023b814
randy3019 <59527893+randy3019@users.noreply.github.com>
2020-11-14 22:50:19 +0800

    Add files via upload

---
+++ Invers_Matrix.m
@@ -0,0 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Nama    : Randy                                           %
+%Nim     : 02171030                                        %
+%PT      : Matematika Institut Teknologi Kalimantan        %
+%Program : MATLAB MENCARI INVERS DARI SUATU MATRIX N X N   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all; %bersihkan nilai var pada workspace
+clc;       %bersihkan command window
+
+B=input('Msukan Jumlah Baris:');     %Input-output
+K=input('Masukan Jumlah Kolom:');
+for i=1:B  %initval:endval
+ for j=1:K
+  a(i,j)=input('Masukan Element Matrix:');
+   end
+end
+a=reshape(a,B,K);   %built-in functions
+inv(a)
+        
