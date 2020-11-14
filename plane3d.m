a3e7e71347bf94d83855c186728a72f2d5cbd23d
randy3019 <59527893+randy3019@users.noreply.github.com>
2020-11-14 22:53:48 +0800

    Update plane3d.m

--- plane3d.m
+++ plane3d.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Nama    : Randy                                           %
-%Nim     : 02171030                                        %
+%Nim     : 02                                              %
 %PT      : Matematika Institut Teknologi Kalimantan        %
 %Program : Proyeksi Persamaan Bidang                       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
